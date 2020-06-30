162c6064a1071f4a2432ff0accf95c3e6d6f8b6f
Syed Shaheer Abbas Jafri <Abbasshaheer93@gmail.com>
2020-06-30 19:15:54 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Syed Shaheer Abbas Jafri and Rumaisa Ahmed are the best friends forever :)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
